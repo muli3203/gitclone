13f77364c26799c3e1e74dc27383f01bc2dd5963
muli3203 <muli3203@gmail.com>
2020-01-18 21:13:25 +0300

    created a service to show result on the initial call of my username

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
+import { ProfileService } from './../profile.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private profileservice: ProfileService) {
+    this.profileservice.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+    });
+   }
 
   ngOnInit() {
   }
