ba20ed358ab8d941db2ca1dea47e2c2ba7888b4c
muli3203 <muli3203@gmail.com>
2020-01-18 20:53:22 +0300

    Imported hhtp client and headers used  the client id and client secret to get information on various gihub users added a function which when prompted returns my user.github.com

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProfileService {
 
-  constructor() { }
+  private username: string;
+  private clientid : '30f91d541ea202a62d16';
+  private clientsecret : '9d274c14ac6b530c569d956aa5cf975a4a407bde';
+
+
+  constructor(private http: Http
+    ) {
+      console.log("service is ready");
+      this.username = 'muli3203'
+     }
+
+     getProfileInfo(){
+       return this.http.get("https://api.github.com/users/" + this.username +"?client_id=" + this.clientid + "&clientsecret-" + this.clientsecret ).map(res=> res.json());
+     }
 }
