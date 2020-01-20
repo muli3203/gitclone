95a290e812d85fcf9b313b3d908cb76022181cb0
muli3203 <muli3203@gmail.com>
2020-01-20 11:23:19 +0300

    created an import of formsmodule from forms module

--- src/app/profile/profile.service/profile.service.ts
+++ src/app/profile/profile.service/profile.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs';
 
 @Injectable({
@@ -8,24 +8,30 @@ import 'rxjs';
 
 export class ProfileService {
 
-  private username: string;
-  private clientid: '30f91d541ea202a62d16';
-  private clientsecret: '9d274c14ac6b530c569d956aa5cf975a4a407bde';
+  username: string;
+  clientid: string;
+  clientsecret: string;
+  httpOptions = {
+    headers: new HttpHeaders({ 'Authorization': 'token 4c76ad7544325a9093b754f55d858b1dd06ca2b5' })
+  };
 
 
-  constructor(private http: HttpClient) {
+  constructor(public http: HttpClient) {
     console.log("service is ready");
     this.username = 'muli3203'
+    // this.clientid = '30f91d541ea202a62d16'
+    // this.clientsecret = "33c3a478a493fde5bdc4c44a3fcc5326f0b22202"
+
   }
 
   getProfileInfo() {
-    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&clientsecret-" + this.clientsecret);
+    return this.http.get("https://api.github.com/users/" + this.username, this.httpOptions);
   }
-  getRepos(){
-    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&clientsecret-" + this.clientsecret);
+  getRepos() {
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos", this.httpOptions);
   }
-  updateProfile(username:string){
-    this.username=username;
+  updateProfile(username: string) {
+    this.username = username;
   }
 }
 
