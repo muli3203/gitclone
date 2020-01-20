05b7c025e2e0064210338060a4488a621b14c958
muli3203 <muli3203@gmail.com>
2020-01-20 09:07:11 +0300

    add function  to get, update and show profile information

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,13 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+   profile: any;
+   repos:any;
+   username: string;
+
   constructor(private profileservice: ProfileService) {
+    this.profileservice.updateProfile(this.username);
     this.profileservice.getProfileInfo().subscribe(profile => {
       console.log(profile);
+      this.profile = profile;
     });
-   }
+
+    this.profileservice.getRepos().subscribe(repos=>{
+      console.log(repos);
+      this.repos = repos;
+    });
+  }
+    findProfile(){
+      this.profileservice.updateProfile(this.username);
+      this.profileservice.getProfileInfo().subscribe(profile => {
+        console.log(profile);
+        this.profile = profile;
+      });
+
+      this.profileservice.getRepos().subscribe(repos=>{
+        console.log(repos);
+        this.repos = repos;
+      });
+    }
 
   ngOnInit() {
+    this.profileservice.updateProfile('muli3203');
+    this.profileservice.getProfileInfo().subscribe(profile=>this.profile = profile);
+    this.profileservice.getRepos().subscribe(repos=>this.repos = repos);
   }
 
 }
