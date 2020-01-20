3a74341cd66506e18c9ccbed768f873b44a470ef
muli3203 <muli3203@gmail.com>
2020-01-20 13:08:18 +0300

    Add   a routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
+import { ProfileComponent } from './profile/profile.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path: "profile", component: ProfileComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
